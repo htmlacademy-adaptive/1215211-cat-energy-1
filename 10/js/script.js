650f45278787b82d80d721974c1a3605b6845883
Keks <keks@htmlacademy.ru>
2024-05-26 20:25:14 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/script.js
@@ -0,0 +1,1 @@
+const siteNavigation=document.querySelector(".site-navigation"),navButton=document.querySelector(".site-navigation__button");siteNavigation.classList.remove("site-navigation--no-js"),navButton.addEventListener("click",(function(){navButton.classList.toggle("site-navigation__button--is-pressed"),siteNavigation.classList.toggle("site-navigation--is-opened")}));
